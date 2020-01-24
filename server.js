a6cb54426ea81dfe175c4bce5172f41bc07517f5
jonesluke1999 <59861784+jonesluke1999@users.noreply.github.com>
2020-01-23 19:52:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -26,13 +26,8 @@ const requestHandler = (request, response) => {
       https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
 
      */
-    try{
-        response.write(listingData);
-        response.end();
-    }
-    catch(err){
-        response.status(404).end();
-        console.log('bad gateway error');
+    if(parsedUrl.pathname == '/listings' && request.method == 'GET'){
+        
     }
 };
 
